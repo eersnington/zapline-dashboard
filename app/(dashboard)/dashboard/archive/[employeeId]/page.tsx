294112ef7b4d6e6c9adee3abaf73f9ae5f45815c
Eers <sreeaadhi07@gmail.com>
2024-01-27 00:50:59 +0530

    minor fixes

--- app/(dashboard)/dashboard/archive/[employeeId]/page.tsx
+++ app/(dashboard)/dashboard/archive/[employeeId]/page.tsx
@@ -1,6 +1,4 @@
 import BreadCrumb from "@/components/breadcrumb";
-import { ProductForm } from "@/components/forms/product-form";
-import React from "react";
 
 export default function Page() {
   const breadcrumbItems = [
@@ -10,14 +8,14 @@ export default function Page() {
   return (
     <div className="flex-1 space-y-4 p-8">
       <BreadCrumb items={breadcrumbItems} />
-      <ProductForm
+      {/* <ProductForm
         categories={[
           { _id: "shirts", name: "shirts" },
           { _id: "pants", name: "pants" },
         ]}
         initialData={null}
         key={null}
-      />
+      /> */}
     </div>
   );
 }
