32a5aa1652c09cc499400f094ea5851b4daba688
Eers <sreeaadhi07@gmail.com>
2024-02-08 23:15:04 +0530

    updated chart

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -17,6 +17,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { Icons } from "@/components/icons";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { Api } from "@/lib/api";
 
 export default async function page() {
   const { getUser } = getKindeServerSession();
@@ -38,6 +39,38 @@ export default async function page() {
       userId: user?.id,
     },
   });
+
+  const call_stats = await Api.get("metrics/get", {
+    user_id: user?.id,
+  }).catch((err) => {
+    return {
+      total_metrics: {
+        total_calls: "None",
+        total_automated_calls: "None",
+        total_transferred_calls: "None",
+        total_abandoned_calls: "None",
+        automated_call_rate: "None",
+        transferred_call_rate: "None",
+        abandoned_call_rate: "None",
+      },
+      weekly_metrics: {
+        "2024-01-01": {
+          total_calls: 5,
+          automated_calls: 4,
+          transferred_calls: 3,
+        },
+      },
+    };
+  });
+
+  const weekly_total = ()=>{
+    var sum = 0;
+    for (const [date, metrics] of Object.entries(call_stats["weekly_metrics"])) {
+      const metrics_dict: any = metrics;
+      sum += metrics_dict.total_calls;
+    }
+    return sum;
+  }
 
   return (
     <ScrollArea className="h-full">
@@ -72,9 +105,11 @@ export default async function page() {
                   <Icons.phoneIncoming className="h-5 w-5 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
-                  <div className="text-2xl font-bold">702</div>
+                  <div className="text-2xl font-bold">
+                    {call_stats["total_metrics"]["total_calls"]}
+                  </div>
                   <p className="text-md text-muted-foreground">
-                    Total calls received this month
+                    Total calls received
                   </p>
                 </CardContent>
               </Card>
@@ -86,9 +121,12 @@ export default async function page() {
                   <Icons.bot className="h-5 w-5 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
-                  <div className="text-2xl font-bold">394</div>
+                  <div className="text-2xl font-bold">
+                    {call_stats["total_metrics"]["total_automated_calls"]}
+                  </div>
                   <p className="text-md text-muted-foreground">
-                    56.13% of total calls
+                    {call_stats["total_metrics"]["automated_call_rate"]} of
+                    total calls
                   </p>
                 </CardContent>
               </Card>
@@ -100,9 +138,12 @@ export default async function page() {
                   <Icons.phoneForward className="h-5 w-5 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
-                  <div className="text-2xl font-bold">282</div>
+                  <div className="text-2xl font-bold">
+                    {call_stats["total_metrics"]["total_transferred_calls"]}
+                  </div>
                   <p className="text-md text-muted-foreground">
-                    40.17% of total calls
+                    {call_stats["total_metrics"]["transferred_call_rate"]} of
+                    total calls
                   </p>
                 </CardContent>
               </Card>
@@ -114,9 +155,12 @@ export default async function page() {
                   <Icons.dollar className="h-5 w-5 text-muted-foreground" />
                 </CardHeader>
                 <CardContent>
-                  <div className="text-2xl font-bold">26</div>
+                  <div className="text-2xl font-bold">
+                    {call_stats["total_metrics"]["total_abandoned_calls"]}
+                  </div>
                   <p className="text-md text-muted-foreground">
-                    3.70% of total calls
+                    {call_stats["total_metrics"]["abandoned_call_rate"]} of
+                    total calls
                   </p>
                 </CardContent>
               </Card>
@@ -127,14 +171,14 @@ export default async function page() {
                   <CardTitle>Overview - Calls Weekly</CardTitle>
                 </CardHeader>
                 <CardContent className="pl-2">
-                  <Overview />
+                  <Overview weeklyMetrics={call_stats["weekly_metrics"]} />
                 </CardContent>
               </Card>
               <Card className="col-span-4 md:col-span-3">
                 <CardHeader>
                   <CardTitle>Recent Calls</CardTitle>
                   <CardDescription>
-                    You received 702 calls this month.
+                    You received {weekly_total()} calls this week.
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
--- components/overview.tsx
+++ components/overview.tsx
@@ -1,58 +1,54 @@
 "use client";
 
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-const data = [
-  {
-    name: "Monday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Tuesday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Wednesday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Thursday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Friday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Saturday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-  {
-    name: "Sunday",
-    total: Math.floor(Math.random() * 10) + 1,
-  },
-];
+interface WeeklyMetrics {
+  [date: string]: {
+    total_calls: number;
+    automated_calls: number;
+    transferred_calls: number;
+  };
+}
 
-export function Overview() {
+interface Props {
+  weeklyMetrics: WeeklyMetrics;
+}
+
+export function Overview({ weeklyMetrics = {} }) {
+  const get_data = () => {
+    return Object.entries(weeklyMetrics).map(([date, metrics]) => {
+      const metrics_dict: any = metrics;
+      return {
+        name: date,
+        total_calls: metrics_dict.total_calls,
+        automated_calls: metrics_dict.automated_calls,
+        transferred_calls: metrics_dict.transferred_calls,
+      };
+    });
+  };
+  console.log(get_data());
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={data}>
-        <XAxis
-          dataKey="name"
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-        />
-        <YAxis
-          stroke="#888888"
-          fontSize={12}
-          tickLine={false}
-          axisLine={false}
-          tickFormatter={(value) => `${value}`}
-        />
-        <Bar dataKey="total" fill="#f97316" radius={[4, 4, 0, 0]} />
+      <BarChart data={get_data()}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="total_calls" fill="#f97316" />
+        <Bar dataKey="automated_calls" fill="#967bb6" />
+        <Bar dataKey="transferred_calls" fill="#FDE767" />
       </BarChart>
     </ResponsiveContainer>
   );
 }
+//
