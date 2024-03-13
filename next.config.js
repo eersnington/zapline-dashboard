d7dcd9d22bbcac7be5d66cba0a40f9533c9a7a32
Sree Narayanan <sreeaadhi07@gmail.com>
2024-03-14 03:34:41 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 1000
 };
 
 module.exports = nextConfig;
